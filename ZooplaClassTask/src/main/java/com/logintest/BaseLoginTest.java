783a92beffcae53e708747d25e771e53ca3738d8
Kawser Mohammad Shafique <kawser.shafique@gmail.com>
2020-05-07 22:08:12 -0400

    Modified Code

--- ZooplaClassTask/src/main/java/com/logintest/BaseLoginTest.java
+++ ZooplaClassTask/src/main/java/com/logintest/BaseLoginTest.java
@@ -1,66 +1,63 @@
 package com.logintest;
 
-import java.util.concurrent.TimeUnit;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 
-public class LoginTest {
+import com.util.Highlighter;
+import com.util.Wait;
 
-	public static void main(String[] args) {
+public class BaseLoginTest {
+
+	public static void main(String[] args) throws Throwable {
 
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+
 		System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
 		Logger.getLogger("org.openqa.selenium").setLevel(Level.OFF);
 
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
 		driver.navigate().to("https://www.zoopla.co.uk");
 		System.out.println("Home Page Title : " + driver.getTitle());
 
-		// *[@class='ui-button-primary ui-cookie-accept-all-medium-large']
 		WebElement MyCookies = driver
 				.findElement(By.xpath("//*[@class='ui-button-primary ui-cookie-accept-all-medium-large']"));
 		MyCookies.click();
 		System.out.println("Page Title after Accept Cookies : " + driver.getTitle());
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		System.out.println("Current Url :" + driver.getCurrentUrl());
 
-		// *[@class='button button--tertiary-dark account-link__text']
 		WebElement SignInButton = driver
 				.findElement(By.xpath("//*[@class='button button--tertiary-dark account-link__text']"));
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("arguments[0].setAttribute('style', 'background: red; border: 2px solid red;');",
-				SignInButton);
+		Highlighter.GetColor(driver, SignInButton);
 		SignInButton.click();
-		System.out.println("Page Title after Click SignIn : " + driver.getTitle());
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		System.out.println("Current Url After SignIn :" + driver.getCurrentUrl());
+		System.out.println("Login Page Title :" + driver.getTitle());
 
-		// *[@type='email']
 		WebElement Email = driver.findElement(By.xpath("//*[@type='email']"));
+		new Wait().getExplicitWait(driver, Email);
+		Highlighter.GetColor(driver, Email, "red");
 		Email.sendKeys("kawser.shafique@gmail.com");
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
-		// *[@type='password']
 		WebElement Password = driver.findElement(By.xpath("//*[@type='password']"));
+		new Wait().getExplicitWait(driver, Password);
+		Highlighter.GetColor(driver, Password, "orange");
 		Password.sendKeys("kawser01");
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
-		// *[@id='signin_submit']
 		WebElement SignIn = driver.findElement(By.xpath("//*[@id='signin_submit']"));
-		js.executeScript("arguments[0].setAttribute('style', 'background: blue; border: 2px solid red;');", SignIn);
+		new Wait().getExplicitWait(driver, SignIn);
+		Highlighter.GetColor(driver, SignIn, "yellow");
 		SignIn.click();
-		System.out.println("Page Title after LogIn : " + driver.getTitle());
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.close();
+		System.out.println("Current Url After Login :" + driver.getCurrentUrl());
+		System.out.println("Page Title after LogIn :" + driver.getTitle());
+
+		driver.quit();
 
 	}
 
